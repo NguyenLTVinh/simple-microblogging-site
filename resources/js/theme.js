b12107873e0a92ee26538072dc8b0ba36ae70294
VinhNguyen2003 <vinhnguyenlethanh@gmail.com>
2025-01-05 09:10:52 -0600

    finished dark mode toggle

--- resources/js/theme.js
+++ resources/js/theme.js
@@ -1,19 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Theme toggle button
     const themeToggleButton = document.getElementById("theme-toggle");
+    const themeIcon = document.createElement("i");
+    themeIcon.id = "theme-icon";
+    themeIcon.classList.add("fas");
+    themeToggleButton.appendChild(themeIcon);
+
     const currentTheme = localStorage.getItem("theme");
+
     if (currentTheme === "dark-mode") {
         document.body.classList.add("dark-mode");
-    }
-    themeToggleButton.addEventListener("click", changeStyle);
-});
-
-function changeStyle() {
-    if (document.body.classList.contains("dark-mode")) {
-        document.body.classList.remove("dark-mode");
-        localStorage.setItem("theme", "default");
+        themeIcon.classList.add("fa-sun");
     } else {
-        document.body.classList.add("dark-mode");
-        localStorage.setItem("theme", "dark-mode");
+        themeIcon.classList.add("fa-moon");
     }
-}
+
+    themeToggleButton.addEventListener("click", () => {
+        if (document.body.classList.contains("dark-mode")) {
+            document.body.classList.remove("dark-mode");
+            localStorage.setItem("theme", "default");
+            themeIcon.classList.remove("fa-sun");
+            themeIcon.classList.add("fa-moon");
+        } else {
+            document.body.classList.add("dark-mode");
+            localStorage.setItem("theme", "dark-mode");
+            themeIcon.classList.remove("fa-moon");
+            themeIcon.classList.add("fa-sun");
+        }
+    });
+});
